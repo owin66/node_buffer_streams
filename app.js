99e00f4bf2aea5a2b301a017e9446ae57f1ca204
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-13 20:01:25 +0300

    Radable_stream

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const fs = require('fs')
+
+const readStream = fs.createReadStream('./docs/text.txt')
+
+readStream.on('data', (chunk) => {
+    console.log('------------')
+    console.log(chunk)
+})
