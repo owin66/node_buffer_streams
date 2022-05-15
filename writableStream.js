5bf5db56b00daba4065441a9d41e597667655fc2
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-15 13:04:23 +0300

    create_handleError

--- writableStream.js
+++ writableStream.js
@@ -3,10 +3,18 @@ const fs = require('fs')
 const readStream = fs.createReadStream('./docs/text.txt')
 const writeStream = fs.createWriteStream('./docs/new-text.txt')
 
-readStream.on('data', (chunk) => {
+/*readStream.on('data', (chunk) => {
     writeStream.write('\n---CHUNK START---\n')
     writeStream.write(chunk)
     writeStream.write('\n---CHUNK END---\n')
-})
-/*
-readStream.pipe(writeStream)*/
+})*/
+const handleError = () => {
+    console.log('Error');
+    readStream.destroy();
+    writeStream.end('Finished with error...')
+}
+
+readStream
+    .on('error', handleError)
+    .pipe(writeStream)
+    .on('error', handleError)
